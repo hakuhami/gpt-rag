a6ad09864ef11bcd63d2f0cfbb8bed1a70160d7d
hakuhami <15sen3haku@gmail.com>
2024-07-29 10:50:59 +0900

    tuning for Japanense sample experiment

--- scripts/run_analysis.py
+++ scripts/run_analysis.py
@@ -4,10 +4,10 @@ from typing import List, Dict
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from src.data_loader import load_annotated_data_from_excel, save_json_data, load_json_data
-from src.data_preprocessor import preprocess_data, split_data
+from src.data_loader import save_json_data, load_json_data
+from src.data_preprocessor import split_data
 from src.rag_model import RAGModel
-from src.evaluator import calculate_f1_scores
+from src.evaluator import evaluate_results
 import yaml
 
 def run_analysis(config_path: str) -> None:
@@ -20,13 +20,9 @@ def run_analysis(config_path: str) -> None:
     # 設定を読み込む
     with open(config_path, 'r') as f:
         config = yaml.safe_load(f)
-
-    # データを読み込む
-    # df = load_json_data(config['raw_data_path/1st_200.json'])
-    # json_data = preprocess_data(df)
     
     #事前に準備したjsonデータを読み込む
-    json_data = load_json_data(config['raw_data_path/1st_200.json'])
+    json_data = load_json_data(config['sample_raw_data_path'])
 
     # データを分割する
     search_data, test_data = split_data(json_data, test_size=config['test_size'])
@@ -48,11 +44,11 @@ def run_analysis(config_path: str) -> None:
     # 予測結果を保存する
     save_json_data(predictions, config['output_path'])
 
-    # F1スコアを計算する
-    f1_scores = calculate_f1_scores(test_data, predictions)
+    # F1スコア、ROUGEスコアを計算する
+    evaluate_scores = evaluate_results(test_data, predictions)
 
     print("F1 Scores:")
-    for element, score in f1_scores.items():
+    for element, score in evaluate_scores.items():
         print(f"{element}: {score:.4f}")
 
 if __name__ == "__main__":
