d6b7536aa2e42cdfa95f7aed80c048b051d315d8
hakuhami <15sen3haku@gmail.com>
2024-09-03 13:22:10 +0900

    update train data

--- scripts/run_analysis.py
+++ scripts/run_analysis.py
@@ -4,7 +4,7 @@ import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from src.data_loader import save_json_data, load_json_data
-from src.data_preprocessor import split_data
+# from src.data_preprocessor import split_data
 from src.rag_model import RAGModel
 from src.evaluator import evaluate_results, save_average_results_to_file
 import yaml
@@ -20,17 +20,25 @@ def run_analysis(config_path: str) -> None:
     # Load the configuration file
     with open(config_path, 'r') as f:
         config = yaml.safe_load(f)
+        
+    # Load the search data from the file
+    with open(config['search_data_path'], 'r', encoding='utf-8-sig') as f:
+        search_data = json.load(f)
+
+    # Load the test data from the file
+    with open(config['test_data_path'], 'r', encoding='utf-8-sig') as f:
+        test_data = json.load(f)
     
-    # Load the pre-prepared JSON data
-    json_data = load_json_data(config['sample_raw_data_path'])
+    # # Load the pre-prepared JSON data
+    # json_data = load_json_data(config['sample_raw_data_path'])
 
-    # Split the data into search and test sets
-    search_data, test_data = split_data(json_data, test_size=config['test_size'])
+    # # Split the data into search and test sets
+    # search_data, test_data = split_data(json_data, test_size=config['test_size'])
 
-    # Save the search and test data
-    save_json_data(search_data, config['search_data_path'])
-    save_json_data(test_data, config['test_data_path'])
-    print("Search and test data is saved.")
+    # # Save the search and test data
+    # save_json_data(search_data, config['search_data_path'])
+    # save_json_data(test_data, config['test_data_path'])
+    # print("Search and test data is saved.")
 
     # Prepare the RAG model with the search data
     rag_model = RAGModel(api_key=config['openai_api_key'], model_name=config['model_name'])
@@ -42,6 +50,7 @@ def run_analysis(config_path: str) -> None:
 
     for item in test_data:
         result = rag_model.analyze_paragraph(item['data'])
+        print(f"Result: {result}")
         result_dict = json.loads(result)
         predictions.append(result_dict)
     print("Analysis is completed.")
