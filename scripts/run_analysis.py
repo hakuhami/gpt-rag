6c17149dcc75989264e1af0d3ac5451a4cf1b08f
hakuhami <15sen3haku@gmail.com>
2024-12-31 03:46:53 +0900

    run explain_separated

--- scripts/run_analysis.py
+++ scripts/run_analysis.py
@@ -5,7 +5,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from src.data_loader import save_json_data, load_json_data
 # from src.data_preprocessor import split_data
-from src.rag_model_8 import RAGModel
+from src.rag_model_9 import RAGModel
 from src.evaluator import evaluate_results, save_average_results_to_file
 import yaml
 import json
