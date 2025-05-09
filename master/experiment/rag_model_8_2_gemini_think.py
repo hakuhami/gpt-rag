d1b88d64bc7019f84812983befb8ff6a18b49ca4
hakuhami <15sen3haku@gmail.com>
2025-05-09 22:40:51 +0900

    [tool]に

--- master/experiment/rag_model_8_2_gemini_think.py
+++ master/experiment/rag_model_8_2_gemini_think.py
@@ -302,7 +302,7 @@ class RAGModel:
         )
         
         config=types.GenerateContentConfig(
-            tools=tool,
+            tools=[tool],
             tool_config=tool_config,
             temperature=0,
             system_instruction="You are an expert in extracting ESG-related promise and their corresponding evidence from corporate reports that describe ESG matters."
@@ -400,7 +400,7 @@ class RAGModel:
         )
         
         config=types.GenerateContentConfig(
-            tools=tool,
+            tools=[tool],
             tool_config=tool_config,
             temperature=0,
             system_instruction="You are an expert in classifying ESG-related promises."
@@ -503,7 +503,7 @@ class RAGModel:
         )
         
         config=types.GenerateContentConfig(
-            tools=tool,
+            tools=[tool],
             tool_config=tool_config,
             temperature=0,
             system_instruction="You are an expert in analyzing ESG-related promise and evidence."
